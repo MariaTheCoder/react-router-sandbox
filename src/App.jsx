2e44d506b0e50b0e9f8c085b4d9e33e939103d31
Maria T. Andersen <mteilmann93@gmail.com>
2023-09-09 12:11:42 +0200

    Import tthe newly created pages. Check out the window.location.pathname property which shows the current pathname. We can use a switch state to ensure that when the current pathname has a certain value, then a specified component is being rendered in our app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import "./App.css";
 import Navbar from "./Navbar";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Pricing from "./pages/Pricing";
 
 function App() {
+  // by taking a look into window.location, we can find the current pathname:
+  // console.log(window.location);
+
+  // Alternatively get only the name of the current path:
+  // console.log(window.location.pathname);
+
+  // We can just take this pathname and use a simple switch state over it
+  let component;
+  switch (window.location.pathname) {
+    case "/":
+      component = <Home />;
+      break;
+    case "/pricing":
+      component = <Pricing />;
+      break;
+    case "/about":
+      component = <About />;
+      break;
+  }
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <div className="container">{component}</div>
+    </>
   );
 }
 
