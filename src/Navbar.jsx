af2fb05cf58d97f9d90378d76f3155dd533362dd
Maria T. Andersen <mteilmann93@gmail.com>
2023-09-09 15:21:13 +0200

    Instead of using a CustomLink component, make use of the NavLink component which comse with the 'react-router-dom' library.

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
@@ -8,29 +8,10 @@ function Navbar() {
         Site Name
       </Link>
       <ul>
-        <CustomLink to="/pricing">Pricing</CustomLink>
-        <CustomLink to="/about">About</CustomLink>
+        <NavLink to="/pricing">Pricing</NavLink>
+        <NavLink to="/about">About</NavLink>
       </ul>
     </nav>
-  );
-}
-
-function CustomLink({ to, children, ...props }) {
-  // The useResolvedPath hook resolves the 'pathname' of the location in the given 'to' value against the pathname of the current location.
-  // In other words, the hook allows you to take a relative or absolute path and combines it with the current path that you are on
-  // and gives you the full absolute path that you will be accessing
-  const resolvedPath = useResolvedPath(to);
-
-  // Add end: true to the object to ensure that the entire path has to match. We do not want partial matches.
-  // That way we are making sure not to compare the pathname to the link of for example /about/career
-  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
-
-  return (
-    <li className={isActive ? "active" : ""}>
-      <Link to={to} {...props}>
-        {children}
-      </Link>
-    </li>
   );
 }
 
