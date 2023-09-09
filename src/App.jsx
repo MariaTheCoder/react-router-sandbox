f015d6222110eebdf18ca8d47f07bdae391b6b27
Maria T. Andersen <mteilmann93@gmail.com>
2023-09-09 13:43:48 +0200

    We can use them to simplify our routing. Instead of having a switch state that looks at the pathname of the current window location, we import Routes and Route from the 'react-router-dom' library insid e of the App component. The Routes component goes into .container and is to contain the different routes that are to be used for this project. This simplifies the code as the switch state is no longer necessary due to the use of React Router.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Navbar";
 import About from "./pages/About";
@@ -5,29 +6,16 @@ import Home from "./pages/Home";
 import Pricing from "./pages/Pricing";
 
 function App() {
-  // by taking a look into window.location, we can find the current pathname:
-  // console.log(window.location);
-
-  // Alternatively get only the name of the current path:
-  // console.log(window.location.pathname);
-
-  // We can just take this pathname and use a simple switch state over it
-  let component;
-  switch (window.location.pathname) {
-    case "/":
-      component = <Home />;
-      break;
-    case "/pricing":
-      component = <Pricing />;
-      break;
-    case "/about":
-      component = <About />;
-      break;
-  }
   return (
     <>
       <Navbar />
-      <div className="container">{component}</div>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
     </>
   );
 }
