d3032cc81ee87f2db8ab4993865a8a747cd28c92
Maria T. Andersen <mteilmann93@gmail.com>
2023-09-09 11:51:31 +0200

    Remember to change the import of the Navbar element now that the component has been moved out of the pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import "./App.css";
-import Navbar from "./pages/Navbar";
+import Navbar from "./Navbar";
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <div>
       <Navbar />
