e0164e9480f6b3146d11e4fc893365bdb62536f5
Maria T. Andersen <mteilmann93@gmail.com>
2023-09-09 12:36:12 +0200

    There are two things one needs to do when using react router. One is to specify which type of roukter one will use and to define the different routes. Here we import the BrowserRouter as we are working with a web application and wrap it around our App component around a BrowserRouter component as all routing happens inside of App

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+// setup which type of router you will be using since we are working with a web application
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// All routing is being managed in the app component. Therefore we wrap our app inside of BrowserRouter
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
